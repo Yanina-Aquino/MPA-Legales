bf831254488f5d64f953ace47f60368e0926db0c
Yanina Aquino <yaninaaquino91@gmail.com>
2024-01-27 21:26:54 -0300

    Initial commit

---
+++ js/forms.js
@@ -0,0 +1,31 @@
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Gracias por contactarte con nosotros, te responderemos en breve!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! Ocurrió un error enviando tu formulario"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Ocurrió un error enviando tu formulario"
+  });
+}
+form.addEventListener("submit", handleSubmit)
